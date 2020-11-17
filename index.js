7159bfdbbb081ca64276da60bf76ea6d9d6e0ad4
Kevin Valias <Kevbodyworks@gmail.com>
2020-11-17 17:17:32 -0600

    initial backend commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const usersRouter = require("./routes/users");
+
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 4001;
+
+app.use(bodyParser.json());
+
+app.use("/users", usersRouter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our API Again!");
+});
+
+app.listen(port, () => {
+  console.log(`Web server is listening on port ${port}!`);
+});
